ff581570c5ed78a627220d2a245719f16ee049f3
Remy Sharp <remy@remysharp.com>
2013-07-12 20:34:59 +0100

    using karma to run tests in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
 
   grunt.initConfig({
     concat: {
@@ -12,15 +12,31 @@ module.exports = function (grunt) {
       }
     },
 
-    jasmine: {
-      compiled: {
-        src: 'dist/$.js',
-        options: {
-          helpers: 'test/helpers.js',
-          specs: 'test/*.js'
-        }
+    karma: {
+      // sadly, the file we test from isn't controllable here (tests src/$.js)
+      options: {
+        configFile: 'karma.conf.js',
+        runnerPort: 9999,
+        browsers: ['Firefox']
+      },
+      all: {
+        browsers: ['Firefox', 'Chrome', 'Safari', 'Opera']
+      },
+      build: {
+        singleRun: true,
+        browsers: ['phantomjs']
       }
     },
+
+    // mocha: {
+    //   compiled: {
+    //     src: 'dist/$.js',
+    //     options: {
+    //       helpers: 'test/helpers.js',
+    //       specs: 'test/*.js'
+    //     }
+    //   }
+    // },
 
     uglify: {
       options: {
@@ -35,12 +51,12 @@ module.exports = function (grunt) {
     watch: {
       src: {
         files: ['src/*.js', 'test/*.js'],
-        tasks: ['concat:build', 'jasmine:compiled']
+        tasks: ['concat:build', 'karma:build:run']
       }
     }
   });
 
-  grunt.registerTask('test', 'jasmine:compiled');
-  grunt.registerTask('build', ['concat:build', 'test', 'uglify:build']);
+  grunt.registerTask('test', 'karma:all');
+  grunt.registerTask('build', ['concat:build', 'karma:build', 'uglify:build']);
   grunt.registerTask('default', ['build']);
 };
